044cf5a8f0cb28a8b7c61c2d0c8bd8c009231886
zfeig <zfeig@126.com>
2018-06-06 14:57:21 +0800

    update config

--- config/index.js
+++ config/index.js
@@ -1,12 +1,15 @@
 'use strict';
 module.exports = {
- mongo:{
-    uri:'mongodb://127.0.0.1:27017/crawl',
-    options:{
-      server:{
-        auto_reconnect:true,
-        poolSize:5
-      }
+    mongo: {
+        uri: 'mongodb://user:password@127.0.0.1:27017/crawl',
+        options: {
+            auth: {
+                authSource: "admin"
+            },
+            server: {
+                auto_reconnect: true,
+                poolSize: 5
+            }
+        }
     }
-  }
 }
